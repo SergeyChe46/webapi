884267b4969190cc4c5f430f4058629c4fd7aee7
SergeyChe46 <sergeyche46@mail.ru>
2023-04-25 16:59:33 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi;
+[Route("[controller]/{action}")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    public ActionResult<string> Get()
+    {
+        return "ActionResult<string>";
+    }
+
+    public ActionResult<Note> GetNote()
+    {
+        Note note = new Note
+        {
+            NoteId = 1,
+            Title = "Title",
+            Description = "Description",
+            CreatedAt = DateTime.Now
+        };
+        return note;
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace webapi;
+public class Note
+{
+    public int NoteId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
