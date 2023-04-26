5f99a4510235d0bcf764dbd1ee329dfeddf743c6
SergeyChe46 <sergeyche46@mail.ru>
2023-04-26 16:28:26 +0300

    Изменена модель.

--- Models/Note.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webapi;
-public class Note
-{
-    public int NoteId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
